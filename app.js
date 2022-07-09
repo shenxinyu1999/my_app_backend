e9407a6d3d35a4319ef0aa9162a431899822d076
Xinyu Shen <shenxinyu1999@outlook.com>
2022-07-08 17:55:03 -0700

    move cors to front, try solve cors error

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+const app = express()
+const cors = require('cors');
+app.use(cors())
+
 require('dotenv').config()
 
 const express = require('express')
-const cors = require('cors');
+
 const bodyParser = require('body-parser')
-const app = express()
+
 const port = process.env.PORT || 3000
 
 const login = require('./routing/login')
-
-app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
