6d51bc8041eee21b2fee6fc688e6591909f8589f
Xinyu Shen <shenxinyu1999@outlook.com>
2022-07-08 17:48:49 -0700

    add process env port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const login = require('./routing/login')
 
